d2bbea1cf227e9c8b0a8a5fd16119aaa150793e7
변정환 <bgyu98@gmail.com>
2022-12-30 15:41:27 +0900

    first test

---
+++ src/main/java/com/example/demo/controller/TestController.java
@@ -0,0 +1,29 @@
+package com.example.demo.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.example.demo.service.TestService;
+import com.example.demo.vo.TestVO;
+
+@Controller
+@RequestMapping("/board")
+public class TestController {
+
+	@Autowired
+	private TestService testService;
+	
+//	@RequestMapping("/{step}")
+//	public String viewPage(@PathVariable String step) {
+//		return "/board/" + step;
+//	}
+	
+	@RequestMapping("/getBoard")
+	public void getBoard(TestVO vo, Model m) {
+		TestVO result = testService.getTest(vo);
+		m.addAttribute("board", result);
+		System.out.println("안녕");
+	}
+}
