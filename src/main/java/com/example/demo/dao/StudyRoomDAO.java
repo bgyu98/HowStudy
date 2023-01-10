0a924ddb57700cc17218b8902ced967f4c65ed33
Soyun Kim <soyun0661@gmail.com>
2023-01-10 19:27:38 +0900

    23/01/10 19:27...

--- src/main/java/com/example/demo/dao/StudyRoomDAO.java
+++ src/main/java/com/example/demo/dao/StudyRoomDAO.java
@@ -1,5 +1,7 @@
 package com.example.demo.dao;
 
+import java.util.List;
+
 import org.apache.ibatis.annotations.Mapper;
 
 import com.example.demo.vo.HistoryVO;
@@ -10,4 +12,10 @@ public interface StudyRoomDAO {
 
 	// 스터디룸 등록
 	public void insertRoom(StudyRoomVO vo);
+
+	// 내가 만든 스터디룸
+	public List<StudyRoomVO> myroomlist(StudyRoomVO vo);
+
+	// 내가 만든 스터디룸에 찍히는 하트
+	public Integer checkheart(Integer sNum);
 }
