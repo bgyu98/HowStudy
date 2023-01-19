1d6b29a784d05879f88edb6fba77f7062baceb96
변정환 <bgyu98@gmail.com>
2023-01-19 10:05:20 +0900

    01/19 10:05

--- src/main/java/com/example/demo/controller/AdminController.java
+++ src/main/java/com/example/demo/controller/AdminController.java
@@ -4,12 +4,10 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.servlet.ModelAndView;
-import org.springframework.ui.Model;
 
-import com.example.demo.dao.UserDAO;
 import com.example.demo.service.FaqService;
 import com.example.demo.service.MembershipService;
 import com.example.demo.service.NoticeService;
